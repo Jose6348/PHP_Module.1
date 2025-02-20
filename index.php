5bd2dddece478d1ec4b96a14c87f8ff8399b745c
Jose6348 <jfalasco88@gmail.com>
2025-02-20 00:19:49 -0300

    O que são variáveis?

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-echo 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+</head>
+
+<body>
+
+    <?php
+    $saudacao = "Oi, ";
+    $Nome = "José Jorge";
+    $titulo = $saudacao . $Nome;
+    $subtitulo = "Seja bem vindo ao meu portfólio";
+    ?>
+
+
+
+    <h1>
+        <?php
+
+
+        echo $titulo;
+
+        ?>
+    </h1>
+
+    <h2>
+        <?=
+        $subtitulo
+        ?>
+    </h2>
+
+</body>
+
+</html>
